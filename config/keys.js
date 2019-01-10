53980a5e46049a039f47d2e2ddca419dd9b3f3d6
Nguyen Le <nathanle89@gmail.com>
2019-01-09 21:54:51 -0800

    Update keys in gitignore

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,8 @@ module.exports = {
   google: {
       id: "71480293123-nheu3ct48mha7i7basreblbarilaae3d.apps.googleusercontent.com",
       secret: "iPFQO9zG_tLcH5kVrv_K9MrP"
+  },
+  database: {
+      host: 'mongodb://emailyadmindev:helloworld1@ds153314.mlab.com:53314/emaily-tinumap-dev'
   }
 };
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('./services/passport');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const { database } = require('./config/keys');
 
 authRoutes(app);
+
+mongoose.connect(database.host);
 
 // Heroku will override this value when deploying
 const PORT = process.env.PORT || 5000;
