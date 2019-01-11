9c019adb92af9a0000640125c2df8b726042e2ba
Nguyen Le <nathanle89@gmail.com>
2019-01-10 22:07:08 -0800

    Add production credentials

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,5 @@
-module.exports = {
-    google: {
-      id: "71480293123-nheu3ct48mha7i7basreblbarilaae3d.apps.googleusercontent.com",
-      secret: "iPFQO9zG_tLcH5kVrv_K9MrP"
-    },
-    database: {
-      host: 'mongodb://emailyadmindev:helloworld1@ds153314.mlab.com:53314/emaily-tinumap-dev'
-    },
-    cookie: {
-        key: 'special_key'
-    }
-};
+if (process.env.NODE_ENV == 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
---
+++ config/prod.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        id: process.env.GOOGLE_ID,
+        secret: process.env.GOOGLE_SECRET
+    },
+    database: {
+        host: process.env.DATABASE_HOST
+    },
+    cookie: {
+        key: process.env.COOKIE_KEY
+    }
+};
