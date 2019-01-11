343d8df6187dc5e924f441c583247ceb387b21ea
Nguyen Le <nathanle89@gmail.com>
2019-01-10 21:40:25 -0800

    More progress on authentication

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-// import models
-require('./models/User');
-
-require('./services/passport');
 const express = require('express');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 const app = express();
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
-const { database } = require('./config/keys');
+const { database, cookie } = require('./config/keys');
 
-authRoutes(app);
+// import models
+require('./models/User');
+require('./services/passport');
 
 mongoose.connect(database.host);
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days expiration
+        keys: [cookie.key]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Heroku will override this value when deploying
 const PORT = process.env.PORT || 5000;
 
+authRoutes(app);
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -3,12 +3,34 @@ const credentials = require('../config/keys');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('mongoose').model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser( (id, done) => {
+   User.findById(id).then( user => {
+       if(user) {
+           done(null, user);
+       }
+   })
+});
+
 passport.use(
     new GoogleStrategy({
         clientID: credentials.google.id,
         clientSecret: credentials.google.secret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        new User({ googleId: profile.id }).save();
+        User.findOne({ googleId: profile.id })
+            .then( (existingUser) => {
+                if (existingUser) {
+                    done(null, existingUser);
+                } else {
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            });
+
     })
 );
