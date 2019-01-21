8b498f9071f82948bc0a9fa1baecedb6b4e5c3d4
nathanle89 <nathanle89@gmail.com>
2019-01-20 22:04:53 -0800

    Setup public assets

--- index.js
+++ index.js
@@ -27,10 +27,22 @@ app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Heroku will override this value when deploying
-const PORT = process.env.PORT || 5000;
-
 authRoutes(app);
 billingRoutes(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like out main.js file, or main.css file!
+    app.use(express.static('client/build'));
+
+    // Expire will server up the index.html file if
+    // it doesn't know the routes
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
+// Heroku will override this value when deploying
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
