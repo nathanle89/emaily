4004075404e947533aa455d17fe51190be758cc3
Nguyen Le <nathanle89@gmail.com>
2019-02-12 22:03:44 -0800

    Implement Survey service

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -4,7 +4,7 @@ const surveyService = require('../services/surveyService');
 module.exports = app => {
     app.post('/api/surveys', requireLogin, async (req, res, next) => {
         try {
-            const survey = await surveyService.create(req.user, {});
+            const survey = await surveyService.create(req.user, req.body);
         } catch (e) {
             next(e)
         }
--- services/surveyService.js
+++ services/surveyService.js
@@ -1,4 +1,4 @@
-const User = require('mongoose').model('surveys');
+const Survey = require('mongoose').model('surveys');
 const customErrors = require('../helpers/customErrors');
 
 module.exports = class SurveyService {
@@ -6,7 +6,20 @@ module.exports = class SurveyService {
         return await Survey.findById(surveyId);
     }
 
-    static async create(user, googleProfile) {
+    static async create(user, surveyData) {
+        const { title, subject, body, recipients } = surveyData;
+
+        const survey = await new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            _user: user.id,
+            dateSent: Date.now()
+        }).save();
+
+        // Handle sending email before saving to DB
+        
         throw new customErrors.ValidationError('Not enough credit');
     }
 };
